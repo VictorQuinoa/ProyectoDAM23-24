55cf042fee709e27e9673022524498f79069d9f2
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-04-29 11:29:34 +0200

    Creacion de getters y setters

--- src/Usuario.java
+++ src/Usuario.java
@@ -15,4 +15,44 @@ public class Usuario {
         this.password = password;
         this.username = username;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
