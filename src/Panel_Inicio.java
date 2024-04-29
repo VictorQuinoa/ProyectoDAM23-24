e29192013f74112c07d62e619e309b8c62dd2f93
yoi <ygarcialago@danielcastelao.org>
2024-04-29 11:57:40 +0200

    Atributos Panel_Inicio.java puestos

--- src/Panel_Inicio.java
+++ src/Panel_Inicio.java
@@ -1,10 +1,22 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JLabel;
-import javax.swing.JButton;
+import javax.swing.*;
 
 public class Panel_Inicio {
 
+    JFrame base_paneles;
+    JPanel inicios_de_sesion;
+    JPanel logo;
+    JPanel botones;
+    JButton inicio_sesion;
+    JButton olvido_Contraseña;
+    JButton registro;
+    JTextField insertar_nombre;
+    JTextField insertar_contraseña;
+    JLabel nombre;
+    JLabel contraseña;
 
+    public Panel_Inicio(){
+
+
+    }
 
 }
