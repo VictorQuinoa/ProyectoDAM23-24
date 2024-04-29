c18d8fa860d837f6fe8f0b680957b3ff64e73077
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-04-29 11:28:57 +0200

    Creacion de Constructores

--- src/Usuario.java
+++ src/Usuario.java
@@ -4,4 +4,15 @@ public class Usuario {
     private String dni;
     private String password;
     private String username;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String apellidos, String dni, String password, String username) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+        this.password = password;
+        this.username = username;
+    }
 }
