9825457cd589c2024616a3d2ea27ac2545901916
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-05-01 14:08:55 +0200

    Creacion de metodos del juego Bacarat

--- src/Juegos/Bacarat.java
+++ src/Juegos/Bacarat.java
@@ -1,18 +1,45 @@
 package Juegos;
+import JuegoDeCartas.Cartas;
+import JuegoDeCartas.Baraja;
+public class Bacarat {
+    private Baraja baraja;
+    private Cartas cartaJugador;
+    private Cartas cartaBanca;
 
-public class Bacarat  {
-
-    private int cartas;
-
-    public void reparto(){
-
+    public Bacarat() {
+        this.baraja = new Baraja();
     }
 
-    public void apuesta(){
-
+    public void repartirCartas() {
+        this.cartaJugador = this.baraja.dealCard();
+        this.cartaBanca = this.baraja.dealCard();
     }
 
-    public void opciones(){
+    private int valorCarta(Cartas carta) {
+        String valor = carta.getValor();
+        switch (valor) {
+            case "Ace":
+                return 1;
+            case "Jack":
+            case "Queen":
+            case "King":
+                return 10;
+            default:
+                return Integer.parseInt(valor);
+        }
+    }
 
+    public String jugar() {
+        repartirCartas();
+        int valorJugador = valorCarta(this.cartaJugador);
+        int valorBanca = valorCarta(this.cartaBanca);
+
+        if (valorJugador > valorBanca) {
+            return "El jugador gana con " + valorJugador + " contra " + valorBanca;
+        } else if (valorJugador < valorBanca) {
+            return "La banca gana con " + valorBanca + " contra " + valorJugador;
+        } else {
+            return "Es un empate con " + valorJugador;
+        }
     }
 }
