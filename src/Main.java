7546a029e161ee21f76d9ce146fe9f56edc1cbe4
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-05-03 13:32:30 +0200

    Prueba del juego del BlackJack en la Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 import Juegos.Bacarat;
+import Juegos.BlackJack;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Bacarat bacarat = new Bacarat();
-        String resultado = bacarat.jugar();
-        System.out.println(resultado);
+//        Bacarat bacarat = new Bacarat();
+//        String resultado = bacarat.jugar();
+//        System.out.println(resultado);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("¡Bienvenido al juego de Blackjack!");
+        BlackJack.jugarBlackjack(scanner);
+        System.out.println("Gracias por jugar al Blackjack. ¡Hasta luego!");
     }
 }
