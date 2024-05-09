e77a2490f828962b09038722014b773593e952c8
yoi <ygarcialago@danielcastelao.org>
2024-05-09 11:24:22 +0200

    Nueva carpeta para las interfaces + interfaz de inicio vuelto a empezar

---
+++ src/View/Panel_Inicio.java
@@ -0,0 +1,41 @@
+package View;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JSeparator;
+import java.awt.Dimension;
+import org.netbeans.lib.awtextra.AbsoluteConstraints;
+import org.netbeans.lib.awtextra.AbsoluteLayout;
+
+public class Panel_Inicio {
+
+    private javax.swing.JPanel Fondo;
+    private javax.swing.JPanel boton_cerrar;
+    private javax.swing.JLabel contraseña;
+    private javax.swing.JLabel foto;
+    private javax.swing.JLabel inicio_sesion;
+    private javax.swing.JPanel inicios_de_sesion;
+    private javax.swing.JPasswordField insertar_contraseña;
+    private javax.swing.JTextField insertar_nombre;
+    private javax.swing.JSeparator linea_contraseña;
+    private javax.swing.JSeparator linea_nombre;
+    private javax.swing.JLabel nombre;
+    private javax.swing.JLabel olvido_contraseña;
+    private javax.swing.JPanel panel_boton_inicio_sesion;
+    private javax.swing.JPanel panel_boton_registrarse;
+    private javax.swing.JPanel panel_botones;
+    private javax.swing.JPanel panel_foto;
+    private javax.swing.JPanel panel_olvido_contraseña;
+    private javax.swing.JLabel registrarse;
+    private javax.swing.JLabel simbolo_cerrar;
+    private javax.swing.JPanel soporte_boton_cerrar;
+    private java.awt.Dimension size_panel_foto;
+    private java.awt.Dimension size_foto;
+
+
+    public Panel_Inicio(){
+
+
+}
