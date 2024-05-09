05305343d9da944730fcae37bead60e049722644
yoi <ygarcialago@danielcastelao.org>
2024-05-09 11:34:48 +0200

    variables ya creadas en el método y definido yá las bases de la ventana

--- src/View/Panel_Inicio.java
+++ src/View/Panel_Inicio.java
@@ -6,36 +6,73 @@ import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JSeparator;
 import java.awt.Dimension;
+//librerias externas para poder usar esos Layouts
 import org.netbeans.lib.awtextra.AbsoluteConstraints;
 import org.netbeans.lib.awtextra.AbsoluteLayout;
 
 public class Panel_Inicio {
 
-    private javax.swing.JPanel Fondo;
-    private javax.swing.JPanel boton_cerrar;
-    private javax.swing.JLabel contraseña;
-    private javax.swing.JLabel foto;
-    private javax.swing.JLabel inicio_sesion;
-    private javax.swing.JPanel inicios_de_sesion;
-    private javax.swing.JPasswordField insertar_contraseña;
-    private javax.swing.JTextField insertar_nombre;
-    private javax.swing.JSeparator linea_contraseña;
-    private javax.swing.JSeparator linea_nombre;
-    private javax.swing.JLabel nombre;
-    private javax.swing.JLabel olvido_contraseña;
-    private javax.swing.JPanel panel_boton_inicio_sesion;
-    private javax.swing.JPanel panel_boton_registrarse;
-    private javax.swing.JPanel panel_botones;
-    private javax.swing.JPanel panel_foto;
-    private javax.swing.JPanel panel_olvido_contraseña;
-    private javax.swing.JLabel registrarse;
-    private javax.swing.JLabel simbolo_cerrar;
-    private javax.swing.JPanel soporte_boton_cerrar;
-    private java.awt.Dimension size_panel_foto;
-    private java.awt.Dimension size_foto;
+    private JFrame base;
+    private JPanel Fondo;
+    private JPanel boton_cerrar;
+    private JLabel contraseña;
+    private  JLabel foto;
+    private JLabel inicio_sesion;
+    private JPanel inicios_de_sesion;
+    private JPasswordField insertar_contraseña;
+    private JTextField insertar_nombre;
+    //usamos los JSeparator para darle un toque bonito dejabo de los fields
+    private JSeparator linea_contraseña;
+    private JSeparator linea_nombre;
+    private JLabel nombre;
+    private JLabel olvido_contraseña;
+    private JPanel panel_boton_inicio_sesion;
+    private JPanel panel_boton_registrarse;
+    private JPanel panel_botones;
+    private JPanel panel_foto;
+    private JPanel panel_olvido_contraseña;
+    private JLabel registrarse;
+    private JLabel simbolo_cerrar;
+    private JPanel soporte_boton_cerrar;
+    private Dimension size_panel_foto;
+    private Dimension size_foto;
+    private Dimension size_base_frame;
 
 
     public Panel_Inicio(){
 
+        base = new JFrame();
+        Fondo = new JPanel();
+        panel_foto = new JPanel();
+        foto = new JLabel();
+        inicios_de_sesion = new JPanel();
+        nombre = new JLabel();
+        contraseña = new JLabel();
+        insertar_contraseña = new JPasswordField();
+        insertar_nombre = new JTextField();
+        linea_nombre = new JSeparator();
+        linea_contraseña = new JSeparator();
+        panel_botones = new JPanel();
+        panel_boton_inicio_sesion = new JPanel();
+        inicio_sesion = new JLabel();
+        panel_boton_registrarse = new JPanel();
+        registrarse = new JLabel();
+        panel_olvido_contraseña = new JPanel();
+        olvido_contraseña = new JLabel();
+        soporte_boton_cerrar = new JPanel();
+        boton_cerrar = new JPanel();
+        simbolo_cerrar = new JLabel();
+        size_panel_foto = new Dimension(1200,618);
+        size_foto = new Dimension(800,200);
+
+        base.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        base.setAlwaysOnTop(true);
+        base.setLocationByPlatform(true);
+        base.setUndecorated(true);
+        base.setResizable(false);
+        base.setVisible(true);
+        base.setSize(new java.awt.Dimension(780, 280));
 
 }
+    }
+
