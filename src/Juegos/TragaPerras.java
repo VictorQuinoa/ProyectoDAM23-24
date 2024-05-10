e21ed13df545e0dae8acd80ea9df163eddbe3559
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-05-10 11:21:55 +0200

    Añadimos el try catch al metodo girar

--- src/Juegos/TragaPerras.java
+++ src/Juegos/TragaPerras.java
@@ -16,27 +16,30 @@ public class TragaPerras {
      * @param apuesta valor de la apuesta insertada
      */
     public void girar(int apuesta) {
-        this.apuesta = apuesta;
-        if (this.apuesta > saldo) {
-            System.out.println("No tienes suficiente saldo para esa apuesta.");
-            return;
-        }
+        try{
+            this.apuesta = apuesta;
+            if (this.apuesta > saldo) {
+                System.out.println("No tienes suficiente saldo para esa apuesta.");
+                return;
+            }
+            saldo -= this.apuesta;
+            Random rand = new Random();
+            String[] resultado = {simbolos[rand.nextInt(simbolos.length)], simbolos[rand.nextInt(simbolos.length)], simbolos[rand.nextInt(simbolos.length)]};
+            System.out.println("Los símbolos son: " + resultado[0] + ", " + resultado[1] + ", " + resultado[2]);
 
-        saldo -= this.apuesta;
-        Random rand = new Random();
-        String[] resultado = {simbolos[rand.nextInt(simbolos.length)], simbolos[rand.nextInt(simbolos.length)], simbolos[rand.nextInt(simbolos.length)]};
-        System.out.println("Los símbolos son: " + resultado[0] + ", " + resultado[1] + ", " + resultado[2]);
-
-        if (resultado[0].equals(resultado[1]) && resultado[0].equals(resultado[2])) {
-            saldo += apuesta * 10;
-            System.out.println("¡Ganaste! Has ganado 10 veces tu apuesta.");
-        } else if (resultado[0].equals(resultado[1]) || resultado[0].equals(resultado[2]) || resultado[1].equals(resultado[2])) {
-            saldo += apuesta * 5;
-            System.out.println("¡Ganaste! Has ganado 5 veces tu apuesta.");
-        } else {
-            System.out.println("No hubo coincidencias. Perdiste tu apuesta.");
+            if (resultado[0].equals(resultado[1]) && resultado[0].equals(resultado[2])) {
+                saldo += apuesta * 10;
+                System.out.println("¡Ganaste! Has ganado 10 veces tu apuesta.");
+            } else if (resultado[0].equals(resultado[1]) || resultado[0].equals(resultado[2]) || resultado[1].equals(resultado[2])) {
+                saldo += apuesta * 5;
+                System.out.println("¡Ganaste! Has ganado 5 veces tu apuesta.");
+            } else {
+                System.out.println("No hubo coincidencias. Perdiste tu apuesta.");
+            }
+            System.out.println("Saldo actual: " + saldo);
+        } catch (Exception e){
+            System.out.println("Error al girar" + e.getMessage());
         }
-        System.out.println("Saldo actual: " + saldo);
     }
 
     /**
