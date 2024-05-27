991d28e35235a170aec70dc2e56d3a86b37e4655
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-05-27 12:37:21 +0200

    test view

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import BaseDeDatos.ConexionBD;
 import Juegos.Bacarat;
+import View.Panel_Inicio;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        Bacarat bacarat = new Bacarat();
-        bacarat.jugarBacarat();
+//        Bacarat bacarat = new Bacarat();
+//        bacarat.jugarBacarat();
 
 //        Scanner scanner = new Scanner(System.in);
 //        System.out.println("¡Bienvenido al juego de Blackjack!");
@@ -14,6 +17,7 @@ public class Main {
 //        tragaperras.jugarTragaperras(tragaperras);
 //        ConexionBD conexionBD = new ConexionBD();
 //        conexionBD.conectar();
+        SwingUtilities.invokeLater(Panel_Inicio::new);
     }
 
 }
