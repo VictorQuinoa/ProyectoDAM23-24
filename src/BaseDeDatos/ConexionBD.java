df01108ea218bc3ac2beefed644b3fb737bf2d3d
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-05-27 12:37:40 +0200

    reduccion de metodos

--- src/BaseDeDatos/ConexionBD.java
+++ src/BaseDeDatos/ConexionBD.java
@@ -18,22 +18,6 @@ public class ConexionBD {
         }
         return connection;
     }
-    public void addUser(String nombre, String apellido_1, String apellido_2, String dni, String password, String username){
-        String SQL = "INSERT INTO Usuario (nombre,apellido_1,apellido_2,dni,password,username) VALUES (?,?,?,?,?,?,?)";
-        try (Connection connection = conectar();
-             PreparedStatement preparedStatement = connection.prepareStatement(SQL)) {
-            preparedStatement.setString(1, nombre);
-            preparedStatement.setString(2, apellido_1);
-            preparedStatement.setString(3, apellido_2);
-            preparedStatement.setString(4, dni);
-            preparedStatement.setString(5, password);
-            preparedStatement.setString(6, username);
-            preparedStatement.executeUpdate();
-            System.out.println("Usuario añadido correctamente.");
-        } catch (SQLException e) {
-            System.out.println("Error al añadir el usuario: " + e.getMessage());
-        }
-    }
 
     public void desconectar(Connection connection) {
         try {
