89f92e41e43b0fa0cc2e8fa719c3f22ba56a1ec9
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-05-27 10:12:27 +0200

    probamos los metodos de la base de datos y del bacarat

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
+import BaseDeDatos.ConexionBD;
 import Juegos.Bacarat;
-import Juegos.BlackJack;
-import Juegos.TragaPerras;
-
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        Bacarat bacarat = new Bacarat();
-//        bacarat.jugarBacarat();
+        Bacarat bacarat = new Bacarat();
+        bacarat.jugarBacarat();
 
 //        Scanner scanner = new Scanner(System.in);
 //        System.out.println("¡Bienvenido al juego de Blackjack!");
 //        BlackJack.jugarBlackjack(scanner);
 //        System.out.println("Gracias por jugar al Blackjack. ¡Hasta luego!");
-        TragaPerras tragaperras = new TragaPerras();
-        tragaperras.jugarTragaperras(tragaperras);
+//        TragaPerras tragaperras = new TragaPerras();
+//        tragaperras.jugarTragaperras(tragaperras);
+//        ConexionBD conexionBD = new ConexionBD();
+//        conexionBD.conectar();
     }
 
 }
