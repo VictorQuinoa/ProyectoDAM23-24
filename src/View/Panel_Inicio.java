714acf2fecfe0efaa8ab53beb43dd83f3873fe56
YoelGarciaLago <145323567+YoelGarciaLago@users.noreply.github.com>
2024-06-01 11:40:02 +0200

    Cambios en música

--- src/View/Panel_Inicio.java
+++ src/View/Panel_Inicio.java
@@ -52,10 +52,11 @@ public class Panel_Inicio extends javax.swing.JFrame {
 
     public Panel_Inicio() {
         initComponents();
-        mf.musicaDeFondo();
+        mf.musicaDeFondo(0);
         panel_boton_registrarse.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
+                dispose();
                 new InsertarDatos();
             }
         });
@@ -108,7 +109,7 @@ public class Panel_Inicio extends javax.swing.JFrame {
 
         panel_foto.setPreferredSize(new java.awt.Dimension(618, 1200));
 
-        foto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Decorativos/Imagenes/Menus|Iconos/logo_Panel_inicio_redimensionado.png")));
+        foto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Decorativos/Imagenes/Menus_Iconos/logo_Panel_inicio_redimensionado.png")));
         foto.setAlignmentY(0.0F);
         foto.setHorizontalTextPosition(SwingConstants.CENTER);
         foto.setPreferredSize(new java.awt.Dimension(800, 1200));
